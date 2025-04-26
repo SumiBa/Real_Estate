3a1211cd1d6cec54e722936079fb124eee249b1b
Sumit Bairagi <sumitbairagi283@gmail.com>
2025-04-26 20:11:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,25 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      // Proxy API requests to Ngrok backend during local development
-      '/get_properties.php': {
-        target: 'https://b795-65-2-31-39.ngrok-free.app', // Ngrok URL for your backend
-        changeOrigin: true, // Needed for virtual hosted sites
-        rewrite: (path) => path.replace(/^\/get_properties.php/, '/get_properties.php'),
-      },
-      '/submit_enquiry.php': {
-        target: 'https://b795-65-2-31-39.ngrok-free.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/submit_enquiry.php/, '/submit_enquiry.php'),
-      },
-      // Proxy for contact.php API
-      '/contact.php': {
-        target: 'https://b795-65-2-31-39.ngrok-free.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/contact.php/, '/contact.php'),
-      }
-    }
-  }
 })
